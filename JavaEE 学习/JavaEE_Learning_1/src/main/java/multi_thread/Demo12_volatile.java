006a88cc5fc5ecd0176b1ce7b8186cf8aa3cc0f8
Kevin Qiu <ess102692@gmail.com>
2025-02-22 12:03:34 +0800

    完成了 volatile 关键字原理的总结

--- JavaEE 学习/JavaEE_Learning_1/src/main/java/multi_thread/Demo12_volatile.java
+++ JavaEE 学习/JavaEE_Learning_1/src/main/java/multi_thread/Demo12_volatile.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Demo12_volatile {
     static int flag = 0;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread myThread_01 = new Thread(() -> {
             while (flag == 0){
 
@@ -12,14 +12,24 @@ public class Demo12_volatile {
             System.out.println(Thread.currentThread().getName() + " has been exited...");
         }, "myThread_01");
 
-        myThread_01.start();
-
         Thread myThread_02 = new Thread(() -> {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Input a number > ");
             flag = scanner.nextInt();
+            System.out.println(Thread.currentThread().getName() + " report: " +"flag = "+flag);
             System.out.println(Thread.currentThread().getName() + " has been exited...");
         }, "myThread_02");
+
+        Thread myThread_03 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " start...");
+            System.out.println(Thread.currentThread().getName() + " report: " +"flag = "+flag);
+            System.out.println(Thread.currentThread().getName() + " has been exited...");
+        }, "myThread_03");
+
+        myThread_01.start();
         myThread_02.start();
+        myThread_02.join(); // 验证线程 2 是否会将 flag 写入主内存中
+        myThread_03.start();
+
     }
 }
